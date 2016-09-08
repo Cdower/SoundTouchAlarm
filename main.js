4eac2d3180ed75b51ba1144b2600b51241ae0c8a
Chris Dower <cliffjumpers57@gmail.com>
2016-09-08 09:51:35 -0700

    removed sspd

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ var httpServer = http.createServer(app);
 var io = require('socket.io')(httpServer);
 var parseString = require('xml2js').parseString;
 var browser = mdns.createBrowser(mdns.tcp('soundtouch'));
-var ssdp = require('node-ssdp-lite')
-  , client = new ssdp();
 var schedule = require('node-schedule');
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );
@@ -17,12 +15,8 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 //require mongodb
 
 ///TODO: store each device in a data base so that the user doesn't have to wait to populate list
-///TODO: remove ssdp, MDNS works so much better
+///TODO:, MDNS works so much better
 ///TODO: Send get request to /info of device when it is found before sending it to the user
-
-client.on('notify', function () {
-      console.log('Got a notification.');
-  });
 
 //***********************************************
 //****************MDNS Block*********************
@@ -149,21 +143,6 @@ function runAlarm(body){///make series of AJAX calls to play music and set volum
   req.write(xmlData);
   req.end('xmlbody');
 }
-
-//*************************************************
-//*****************SSPD Block**********************
-
-client.on('response', function inResponse(msg, rinfo) {
-  console.log('Got a response to an m-search.');
-  console.log(rinfo);
-  if(rinfo.address !== global.address){
-    global.address=rinfo.address;
-    emitSpeakerInfo(rinfo.address);
-  }
-});
-
-client.search('urn:schemas-upnp-org:device:MediaRenderer:1');
-console.log('SSDP query');
 
 //*************************************************
 
